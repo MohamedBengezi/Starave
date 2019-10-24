3ec249637b1c8cf9cd2cb0b2bad2c4205608d6c6
Mohamed <mohamed@mohameds-mbp.war.can.ibm.com>
2019-10-24 12:32:19 -0400

    comments for results.js

--- src/js/results.js
+++ src/js/results.js
@@ -5,14 +5,15 @@ function initMap() {
     var modrn = { lat: 43.2584358, lng: -79.877296 };
     var dd = { lat: 43.256571, lng: -79.8670827 };
     var trust = { lat: 43.2584397, lng: -79.8794847 };
-    // The map, centered at Uluru
+    // The map, centered at a location on main st
     var map = new google.maps.Map(
         document.getElementById('map'), { zoom: 13, center: hamilton });
-    // The marker, positioned at Uluru
+    // The markers, positioned at each club defined above
     var mdrnMarker = new google.maps.Marker({ position: modrn, map: map });
     var ddMarker = new google.maps.Marker({ position: dd, map: map });
     var trustMarker = new google.maps.Marker({ position: trust, map: map });
 
+    //A sample description to display in the label
     var contentString = '<div id="content">' +
         '<div id="siteNotice">' +
         '</div>' +
@@ -27,19 +28,21 @@ function initMap() {
         '</div>' +
         '</div>';
 
+    //Creating an infowindow (AKA label popup) with the content being the description above
     var infowindow = new google.maps.InfoWindow({
         content: contentString
     });
 
-
+    //On click of the modrn marker, open the infowindow
     mdrnMarker.addListener('click', function () {
         infowindow.open(map, mdrnMarker);
     });
 
+    //On click of the dd marker, open the infowindow
     ddMarker.addListener('click', function () {
         infowindow.open(map, ddMarker);
     });
-
+    //On click of the trust marker, open the infowindow
     trustMarker.addListener('click', function () {
         infowindow.open(map, trustMarker);
     });
