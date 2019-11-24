246f5a0ded8b62ba74af8d28cfece75ee3cdb298
Amit binu <binua@mcmaster.ca>
2019-11-24 12:53:20 -0500

    Cleaning up the code

---
+++ src/php/footer.php
@@ -0,0 +1,7 @@
+ <!-- The footer section -->
+    <footer>
+        <a href="../sitemap.xml">Sitemap</a>
+    </footer> >
+
+</body>
+</html>
---
+++ src/php/navigationMenu.php
@@ -0,0 +1,31 @@
+ <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../home.php">
+                            Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./userRegistration.php">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./objectRegistration.php">Add Club</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Contact Us</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0" action="search.html">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+
