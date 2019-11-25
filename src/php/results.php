a12062d5cb8449a148b802c05001f6b621e691ea
Amit binu <binua@mcmaster.ca>
2019-11-25 18:47:00 -0500

    Made the page more dynamic

--- src/php/results.php
+++ src/php/results.php
@@ -27,10 +27,41 @@
 				$name = $rows[$i]['NAME'];
     				printError($name);
 
-
+           
+            echo '<a href="./clubs/modrn.html" class="list-group-item list-group-item-action flex-column align-items-start h-100">';
+            echo '   <div class="d-flex w-100 justify-content-between">';
+            echo '       <h5 class="mb-1 ml-3">',$rows[$i]['NAME'],'</h5>';
+            echo '    </div>';
+            echo '    <!-- Adding an image of the club -->';
+            echo '    <div>';
+            echo '        <img class="club-image" src="',$rows[$i]['IMAGE'],'" alt="Modrn Thumbnail" />';
+            echo '    </div>';
+            echo '    <div class="desc mr-5">';
+            echo '        <p class="mb-1">',$rows[$i]['DESCRIPTION'],'</p>';
+            echo '    </div>';
+            echo "    <!-- spacing out the ratings -->"; 
+            echo "    <br>";
+            echo "    <br>";
+            echo "    <br>";
+            echo "    <br>";
+            echo "    <br>";
+            echo "    <br>";
+            echo "    <br>";
+            echo "    <br>";
+            echo "    <!-- using font-awesome library to display the star ratings -->";
+            echo '    <div class="rating mr-3">';
+	    $numberOfStars=$rows[$i]['RATING'];
+	    for($i=0; $i<$numberOfStars; $i++){
+            	echo '        <span class="fa fa-star checked"></span>';
+	    }
+	    $noStars=5-$numberOfStars;
+	    for($i=0; $i<$noStars; $i++){
+           	 echo '        <span class="fa fa-star"></span>';                
+            }
+            echo "     </div>";
+	    echo "</a>";
 			}
 		?>
-                 	
 <a class="list-group-item list-group-item-action flex-column align-items-start h-100">
                 <div id="map"></div>
                 <script type="text/javascript" src="../js/results.js"> </script>
