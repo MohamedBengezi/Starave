3dd9dabe04b03ae2db526637dc5d29b401b29067
MohamedBengezi <bengezim@mcmaster.ca>
2019-11-28 17:56:23 +0000

    Moving home.php to php folder and cleaning it up

---
+++ src/php/home.php
@@ -0,0 +1,19 @@
+<?php include "../../../inc/dbinfo.inc"; ?>
+<?php include "./helpers/functions.php"; ?>
+<html lang="en">
+<?php include "./header.php"; ?>
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="../css/main.css" />
+</head>
+
+<body>
+    <?php include "./navigationMenu.php"; ?>
+  <footer>
+    <a href="../../sitemap.xml">Sitemap</a> <!-- This is the footer that will be in all the pages -->
+  </footer> 
+</body>
+</html>
+
--- src/php/navigationMenu.php
+++ src/php/navigationMenu.php
@@ -1,3 +1,7 @@
+<?php include "../../../inc/dbinfo.inc"; ?>
+<?php
+    session_start();
+?>
  <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -8,7 +12,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="../home.php">
+                        <a class="nav-link" href="../php/home.php">
                             Home
                         </a>
                     </li>
@@ -21,8 +25,13 @@
                     <li class="nav-item">
                         <a class="nav-link" href="">Contact Us</a>
                     </li>
+              <?php if (isset($_SESSION['ID'])) { ?>
+                    <a class="nav-link" href="helpers/logout.php">Logout</a>
+              <?php }else{ ?>
+                    <a class="nav-link" href="helpers/login.php">Login</a>
+              <?php } ?>
                 </ul>
-                <form class="form-inline my-2 my-lg-0" action="search.html">
+                <form class="form-inline my-2 my-lg-0" action="search.php">
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
